bd190f1642da0f0995a891ab29145e992c20f98c
Eitan <PauleitanWebb@gmail.com>
2025-09-12 19:38:55 +1000

    Re-Wrote the almost all the code to improve readibility

---
+++ NoDeletingRootDirectory.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+searchString = ''
+
+fileTypes = {
+    1: '*.TXT',
+    2: '*.PDF',
+    3: '*.MD',
+    4: '*.EXE',
+    5: '*.PY',
+    6: '*.SH',
+    7: '*.PDF',
+    8: '*.JPG',
+    9: '*.MP4',
+    0: '*.DOCZ',   
+}
+detectedFiles = {}
+def targetFile():
+    while True:
+        for key in sorted(fileTypes):
+            print(f"{key}: {fileTypes[key]}")
+
+        userInput = input('Choose a file type: ')
+        if userInput.isdigit():
+            result = fileTypes.get(int(userInput))
+            if result:  
+                print(f"You selected: {result}")
+                return result  
+            else:
+                print("Invalid number, please choose from the menu.")
+        else:
+            print("Invalid input, please enter a number from the menu.")
+
+selectedFile = targetFile()
+
+def searchFile(selectedFile):
+    searchPath = Path(input('Please enter path to search: '))
+
+    if not searchPath.exists() or not searchPath.is_dir():
+        print("The path does not exist or is not a directory. Exiting search.")
+        return
+
+    for file_path in searchPath.rglob('*'):
+     if file_path.suffix.upper() == selectedFile[1:]: 
+        print(file_path)
+        file_path
+
+searchFile(selectedFile)  
